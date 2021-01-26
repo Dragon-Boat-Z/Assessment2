ee75f0cd85640c8532cb876869df36ec1f7746e2
jamesrwilkinson <jrm567@york.ac.uk>
2021-01-26 20:20:21 +0000

    testing setup

--- core/src/com/hardgforgif/dragonboatracing/testing/BoatTest.java
+++ core/src/com/hardgforgif/dragonboatracing/testing/BoatTest.java
@@ -1,10 +1,24 @@
 package com.hardgforgif.dragonboatracing.core.testing;
 
-@DisplayName("")
-public class TestBoat {
-    
-}
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
-test {
-    useJUnitPlatform()
+@DisplayName("Test Boat Class")
+public class TestBoat {
+
+    Lane mockLane = Mockito.mock(Lane.class);
+
+    @Before
+    public void setUp(){
+        Boat testBoat = new Boat(120f, 90f, 100f, 110f, 3, mockLane);
+    }
+
+    @Test
+    public void testBoatConstructor(){
+        assertEquals(120f, testBoat.getRobustness(), "Yo sort out the spaghetti code loser!");
+    }
 }
