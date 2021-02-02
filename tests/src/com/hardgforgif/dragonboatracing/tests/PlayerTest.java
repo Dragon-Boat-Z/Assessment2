7c8ebd8378e6076620f5b5f036d8f6d6aca1b717
Will <44756389+TheWill10m@users.noreply.github.com>
2021-02-02 20:04:11 +0000

    PlayerTest complete

--- tests/src/com/hardgforgif/dragonboatracing/tests/PlayerTest.java
+++ tests/src/com/hardgforgif/dragonboatracing/tests/PlayerTest.java
@@ -27,10 +27,10 @@ public class PlayerTest {
     float rightTurningAngle = 90f;
     float leftTurningAngle = -90f;
     boolean keysPressed[];
-    boolean[] turnRight = new boolean[]{true, true, false, false};
-    boolean[] turnLeft = new boolean[]{false, false, true, true};
+    boolean[] turnRight = new boolean[]{false, true, false, false};
+    boolean[] turnLeft = new boolean[]{false, false, false, true};
     boolean[] moveUp = new boolean[]{true, false, false, false};
-    boolean[] moveDown = new boolean[]{false,false,false,true};
+    boolean[] moveDown = new boolean[]{false,false,true,false};
     boolean[] noMovement = new boolean[]{false,false,false,false};
 
 
@@ -45,8 +45,7 @@ public class PlayerTest {
         testPlayer = new Player(robustness, speed, acceleration, maneuverability, boatType, mockLane);
         World world;
         world = new World(new Vector2(0f, 0f), true);
-        //world.setContactListener(new ContactListener());
-        //testPlayer.createBoatBody(world, 100f, 120f, "Boat1.json");
+        testPlayer.createBoatBody(world, 100f, 120f, "Boat1.json");
     }
 
     @Test
@@ -59,21 +58,37 @@ public class PlayerTest {
         assertEquals(mockLane, testPlayer.getLane());
     }
 
-    // @Test
+    @Test
     //Struggling to pass through the keys pressed to the instance method updatePlayer
-    // public void testTargetingAngle(){
-        // testPlayer.updatePlayer(turnRight, 2f);
-        // assertEquals(rightTurningAngle, testPlayer.getTargetAngle());
-        // testPlayer.updatePlayer(turnLeft, 2f);
-        // assertEquals(leftTurningAngle, testPlayer.getTargetAngle());
-        // testPlayer.updatePlayer(noMovement,2f);
-        // assertEquals(0f, testPlayer.getTargetAngle());
-    // }
+    public void testUpdatePlayerTurning(){
+        testPlayer.updatePlayer(turnRight, 2f);
+        assertEquals(rightTurningAngle, testPlayer.getTargetAngle());
+        testPlayer.updatePlayer(turnLeft, 2f);
+        assertEquals(leftTurningAngle, testPlayer.getTargetAngle());
+        testPlayer.updatePlayer(noMovement,2f);
+        assertEquals(0f, testPlayer.getTargetAngle());
+    }
 
-    // @Test
-    // public void testVerticalBoatMovement(){
-        //testPlayer.updatePlayer(moveUp, 1f);
-        //assertEquals();
-        //testPlayer.updatePlayer(moveDown, 1f);
-    // }
+    @Test
+    public void testUpdatePlayerMove(){
+        float speedChange = 0.15f * ((acceleration * 0.8f)/90)  * (120f / 100);
+        float expectedSpeed;
+        float actualSpeed;
+
+        testPlayer.updatePlayer(moveUp, 1f);
+        expectedSpeed = (float) Math.round((20f + speedChange) * 10) / 10;
+        actualSpeed = (float) Math.round(testPlayer.getCurrentSpeed() * 10) / 10;
+        assertEquals(expectedSpeed, actualSpeed);
+        assertEquals(2, 2);
+
+        testPlayer.updatePlayer(moveDown, 1f);
+        testPlayer.updatePlayer(moveDown, 1f);
+        expectedSpeed = (float) Math.round((20f - speedChange) * 10) / 10;
+        actualSpeed = (float) Math.round(testPlayer.getCurrentSpeed() * 10) / 10;
+        assertEquals(expectedSpeed, actualSpeed);
+
+        testPlayer.updatePlayer(noMovement,1f);
+        actualSpeed = (float) Math.round(testPlayer.getCurrentSpeed() * 10) / 10;
+        assertEquals(expectedSpeed, actualSpeed);
+    }
 }
