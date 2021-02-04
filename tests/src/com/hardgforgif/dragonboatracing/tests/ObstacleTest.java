ce243a494955ce1bf25e140137d12bff5c64de2e
Will <44756389+TheWill10m@users.noreply.github.com>
2021-02-04 11:42:38 +0000

    ObstacleTest done

--- tests/src/com/hardgforgif/dragonboatracing/tests/ObstacleTest.java
+++ tests/src/com/hardgforgif/dragonboatracing/tests/ObstacleTest.java
@@ -13,12 +13,13 @@ import com.badlogic.gdx.physics.box2d.World;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 
 @RunWith(GdxTestRunner.class)
 public class ObstacleTest {
     
     Obstacle testObstacle;
-    World mockWorld;
+    World world;
 
     String textureName = "Obstacles/Obstacle1.png";
 
@@ -28,14 +29,25 @@ public class ObstacleTest {
         Gdx.gl20 = Mockito.mock(GL20.class);
         Gdx.gl30 = Mockito.mock(GL30.class);
 
-        //mockWorld = Mockito.mock(World.class);
+        
+        world = new World(new Vector2(0f, 0f), true);
 
         testObstacle = new Obstacle(textureName);
+        testObstacle.createObstacleBody(world, 100f, 120f, "Obstacles/Obstacle1.json", -0.8f);
     }
 
     @Test
     public void testObstacleConstructor(){
         Texture texture = testObstacle.getObstaclTexture();
-        assertTrue((texture instanceof Texture) && texture != null);
+        assertEquals("Obstacles/Obstacle1.png", texture.toString());
+    }
+
+    @Test
+    public void testCreateObstacleBody(){
+        assertEquals(0.19999999f, testObstacle.getObstacleSprite().getScaleX());
+        assertEquals(0.19999999f, testObstacle.getObstacleSprite().getScaleY());
+        assertEquals(new Vector2(100f, 120f), testObstacle.getObstacleBody().getPosition());
+        assertEquals(9866f, testObstacle.getObstacleSprite().getX());
+        assertEquals(11892, testObstacle.getObstacleSprite().getY());
     }
 }
